37b72bd302ac2db37a17305026017a86d1489fa9
elealarconfra <82480483+elealarconfra@users.noreply.github.com>
2022-12-27 22:16:27 -0500

    switch  skill, array JSON skill, ins jquery

--- skills.js
+++ skills.js
@@ -1,4 +1,88 @@
-function openPython(){
-    window.open("description.html");
-
+function skillDetail(skillId){    
+    switch(skillId.id){
+        case "1":
+            window.open("python.html");   
+            break;
+        case "2":
+            window.open("java.html");     
+            break;
+        case "3":
+            window.open("html.html");     
+            break;
+        case "4":
+            window.open("css.html");     
+            break;
+        case "5":
+            window.open("javascript.html");     
+            break;
+        case "6":
+            window.open("javascript2.html");     
+            break;
+        case "7":
+            window.open("javascript3.html");     
+            break;
+        case "8":
+            window.open("javascript4.html");     
+            break;
+        case "9":
+            window.open("javascript5.html");     
+            break;    
+    }
 }
+[
+    {
+        "id":1,
+        "name":"python",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":2,
+        "name":"java",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":3,
+        "name":"html",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":4,
+        "name":"css",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":5,
+        "name":"javascript",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":6,
+        "name":"javascript2",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":7,
+        "name":"javascript3",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":8,
+        "name":"javascript4",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+    {
+        "id":9,
+        "name":"javascript5",
+        "description":"Lorem ipsum, dolor sit amet consectetur adipisicing elit. At consequuntur iusto expedita odit veritatis a vel esse nam reiciendis qui, officiis quae et obcaecati, dicta autem, consequatur cum excepturi error."
+
+    },
+]
+    
